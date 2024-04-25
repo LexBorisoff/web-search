60ba50e2cc764def130d486d3a7de21e29a09411
Lex Borisoff <lex@borisoff.dev>
2024-04-24 20:42:06 -0400

    added matchers export to index.js

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './browser/browser.js';
 export * from './engine/engine.js';
 export * from './browser/browser.types.js';
 export * from './engine/engine.types.js';
+export * from './matchers/matchers.js';
