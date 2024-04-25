edb882b6bcfe4f3f967bcef09adf26f438823f0c
Lex Borisoff <lex@borisoff.dev>
2024-04-24 22:05:29 -0400

    fixed package.json conditional exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * from './browser/browser.js';
 export * from './engine/engine.js';
 export * from './browser/browser.types.js';
 export * from './engine/engine.types.js';
-export * from './matchers/matchers.js';
